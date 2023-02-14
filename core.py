54ebcf6f3ae1beecc3fbbcb48156b25bf3a8afb1
Beksultan <96925396+Vazno@users.noreply.github.com>
2023-02-14 08:19:21 +0600

    Used threading to remove loss of clicks
    Delted restart, quit binds

--- core.py
+++ core.py
@@ -1,18 +1,16 @@
 from random import choice
 from typing import Tuple
+import threading
 import time
 from keyboard import is_pressed
-import sys
 from copy import deepcopy
 
 from utils import has_duplicate_lists, cls
 
-BINDS = {"quit": "q",
-"up": "w",
-"down": "s",
-"left": "a",
-"right": "d",
-"restart": "r"}
+BINDS = {"up": "w",
+		"down": "s",
+		"left": "a",
+		"right": "d",}
 
 
 class Piece:
@@ -41,31 +39,54 @@ class Game:
 		self.matrix = list()
 		self.__generate_matrix()
 		self.apple_spawn_repeat = 5 # Tries to spawn apple every x second
-		self.snake_movement_repeat = 0.2
+		self.snake_movement_repeat = 0.2 # Snake's speed
 
 
 		self.snake_moved = False
-		self.snake_positions = list()
-		self.snake_positions.append([-1,0])
-		self.snake_positions.append([-2,0])
-		self.snake_positions.append([-3,0])
-		self.snake_positions.append([-4,0])
-		self.snake_positions.append([-5,0])
+		# Snakes spawn position
+		self.snake_positions = [[-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0]]
 
 		self.snake_direction = "up"
 
-		self.cant_go_up = False
-		self.cant_go_down = True
-		self.cant_go_left = False
-		self.cant_go_right = False
-
+		# Says how many apples were collected, and snake's body should grow to that x number
 		self.collected_apples_length = 0
+
+
+	def set_snake_direction_limits(self):
+		if self.snake_direction == "up":
+			self.cant_go_up = False
+			self.cant_go_down = True
+			self.cant_go_left = False
+			self.cant_go_right = False
+		elif self.snake_direction == "down":
+			self.cant_go_up = True
+			self.cant_go_down = False
+			self.cant_go_left = False
+			self.cant_go_right = False
+		elif self.snake_direction == "right":
+			self.cant_go_up = False
+			self.cant_go_down = False
+			self.cant_go_left = True
+		elif self.snake_direction == "left":
+			self.cant_go_up = False
+			self.cant_go_down = False
+			self.cant_go_left = False
+			self.cant_go_right = True
+
+	@property
+	def size(self):
+		return self._size
+	
+	@size.setter
+	def size(self, x: Tuple[int, int]):
+		self._size = x
+		self.__generate_matrix()
 
 	def gameover(self):
 		self.run = False
 
-
 	def move_snake(self):
+		self.set_snake_direction_limits()
 		# Deleting moved tail of snake
 		if len(self.snake_positions) > 1 and self.snake_moved and self.collected_apples_length == 0:
 			self.matrix[self.snake_positions[0][0]][self.snake_positions[0][1]] = self.graphic.empty_cell
@@ -75,10 +96,6 @@ class Game:
 			self.collected_apples_length -= 1
 
 		if self.snake_direction == "up":
-			self.cant_go_up = False
-			self.cant_go_down = True
-			self.cant_go_left = False
-			self.cant_go_right = False
 			try:
 				self.matrix[self.snake_positions[-1][0]-1][self.snake_positions[-1][1]]
 				self.snake_positions.append([self.snake_positions[-1][0]-1, self.snake_positions[-1][1]])
@@ -87,10 +104,6 @@ class Game:
 				self.snake_positions.append([-1, self.snake_positions[-1][1]])
 
 		if self.snake_direction == "down":
-			self.cant_go_up = True
-			self.cant_go_down = False
-			self.cant_go_left = False
-			self.cant_go_right = False
 			if len(self.matrix)-1 >= self.snake_positions[-1][0]+1:
 				self.snake_positions.append([self.snake_positions[-1][0]+1, self.snake_positions[-1][1]])
 			else:
@@ -98,10 +111,6 @@ class Game:
 				self.snake_positions.append([0, self.snake_positions[-1][1]])
 
 		if self.snake_direction == "right":
-			self.cant_go_up = False
-			self.cant_go_down = False
-			self.cant_go_left = True
-			self.cant_go_right = False
 			if len(self.matrix[self.snake_positions[-1][0]])-1 >= self.snake_positions[-1][1]+1:
 				self.snake_positions.append([self.snake_positions[-1][0], self.snake_positions[-1][1]+1])
 			else:
@@ -109,10 +118,6 @@ class Game:
 				self.snake_positions.append([self.snake_positions[-1][0], 0])
 
 		if self.snake_direction == "left":
-			self.cant_go_up = False
-			self.cant_go_down = False
-			self.cant_go_left = False
-			self.cant_go_right = True
 			try:
 				self.matrix[self.snake_positions[-1][0]][self.snake_positions[-1][1]-1]
 				self.snake_positions.append([self.snake_positions[-1][0], self.snake_positions[-1][1]-1])
@@ -132,17 +137,6 @@ class Game:
 				self.collected_apples_length += 1
 			self.matrix[position[0]][position[1]] = self.graphic.snake
 
-
-
-	@property
-	def size(self):
-		return self._size
-	
-	@size.setter
-	def size(self, x: Tuple[int, int]):
-		self._size = x
-		self.__generate_matrix()
-
 	def __generate_matrix(self):
 		'''Generates the board'''
 		self.matrix = list()
@@ -150,9 +144,9 @@ class Game:
 			self.matrix.append(list())
 			for x in range(self.size[0]):
 				self.matrix[-1].append(self.graphic.empty_cell)
-	
+
 	def refresh_matrix(self):
-		'''Use it every time when you are editing looks of pieces'''
+		'''Refreshes the matrix, and replaces the .look attribute of piece'''
 		for row in self.matrix:
 			for piece in row:
 				# Replacing Piece with the new Piece that has different attribute look
@@ -175,12 +169,6 @@ class Game:
 			y, x = choice(empty_cells)
 			self.matrix[y][x] = self.graphic.apple
 
-	def create_matrix_string(self) -> str:
-		text = "\n"
-		for row in self.matrix:
-			text += "".join([str(p) for p in row]) + "\n"
-		return text
-
 	def print_matrix(self):
 		cls()
 		text = "\n"
@@ -188,27 +176,31 @@ class Game:
 			text += "".join([str(p) for p in row]) + "\n"
 		print(text)
 
-	def change_direction(self):
-		if is_pressed(BINDS['up']):
-			if not self.cant_go_up:
-				self.snake_direction = "up"
+	def button_loop(self):
+		while True:
+			if is_pressed(BINDS['up']):
+				if not self.cant_go_up:
+					self.snake_direction = "up"
 
-		if is_pressed(BINDS['down']):
-			if not self.cant_go_down:
-				self.snake_direction = "down"
+			if is_pressed(BINDS['down']):
+				if not self.cant_go_down:
+					self.snake_direction = "down"
 
-		if is_pressed(BINDS['left']):
-			if not self.cant_go_left:
-				self.snake_direction = "left"
+			if is_pressed(BINDS['left']):
+				if not self.cant_go_left:
+					self.snake_direction = "left"
 
-		if is_pressed(BINDS['right']):
-			if not self.cant_go_right:
-				self.snake_direction = "right"
+			if is_pressed(BINDS['right']):
+				if not self.cant_go_right:
+					self.snake_direction = "right"
 
 	def mainloop(self, tick: int = 0.01):
 		self.default_settings = deepcopy([self.graphic,
 							self.snake_direction, self.snake_positions,
 							self.size, self.collected_apples_length])
+		
+		self.set_snake_direction_limits()
+
 		apple_timer = 0
 		snake_movement_timer = 0
 		self.run = True
@@ -216,35 +208,25 @@ class Game:
 			self.print_matrix()
 			time.sleep(tick)
 
-
 			# Apple generation
 			apple_timer += tick
 			if apple_timer >= self.apple_spawn_repeat:
 				apple_timer = 0
 				self.generate_apples()
-			
-			snake_movement_timer += tick
+
 			# Snake movement
+			snake_movement_timer += tick
 			if snake_movement_timer >= self.snake_movement_repeat:
 				snake_movement_timer = 0
 				self.move_snake()
-
-			self.change_direction()
-
-			if is_pressed(BINDS['quit']):
-				print('You left the game!')
-				sys.exit()
-			elif is_pressed(BINDS['restart']):
-				self.graphic = self.default_settings[0]
-				self.snake_direction = self.default_settings[1]
-				self.snake_positions =  self.default_settings[2]
-				self.size = self.default_settings[3]
-				self.collected_apples_length = self.default_settings[4]
-				self.mainloop(tick)
 		print("You lost")
 
 if __name__ == "__main__":
 	game = Game((70, 30), Graphic("*", "x", "@"))
+	game.snake_movement_repeat = 0.015
+	game.apple_spawn_repeat = 0.1
 
-	game.print_matrix()
-	game.mainloop()
+	t1 = threading.Thread(target=game.mainloop, args=[0.01])
+	t1.start()
+	t2 = threading.Thread(target=game.button_loop)
+	t2.start()
